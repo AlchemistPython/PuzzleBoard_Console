c74faa4b2f0e301835b9432c21be33596f3e0d53
David Isaac Alvarez Garcia <alchemistcodepython@gmail.com>
2021-03-02 16:39:11 -0600

    Eliminate dict and make decorators to print the board

--- puzzleboard.py
+++ puzzleboard.py
@@ -1,59 +1,71 @@
-class Puzzle:
-    
-    def __init__(self):
-        self.array = list()
-        self.dict = dict()
-        self.account = 0
-    
-    def fillPuzzle(self):
-        """Method that fill the list of lists"""
-        # account = self.account
-        newlist = list()
-        for account, number in enumerate(range(1,17),start=1):
-            number = str(number) if number != 16 else 'X'
-            newlist.append(number)
-            if account in (4,8,12,16):
-                self.array.append(newlist)
-                newlist = list()
-        return self.array
-    
-    def shuffleItems(self):
-        """Method that shuffle a list of lists """
-        from random import shuffle
-        # shuffle(self.array)
-        for sublist in self.array:
-            shuffle(sublist)
-        return self.array
-    
-    def establishKeys(self):
-        account = self.account
-        for boxes in self.array:
-            for account, element in enumerate(boxes,start=account+1):
-                self.dict.setdefault(account,element)
-            account = account
-        return self.dict
-        
-    def showArray(self):
-        print(f"\nLista : {self.array}")
-    
-    def showPuzzle(self):
-        print("-----------------------".rjust(4))
-        for account, value in enumerate(self.dict.values()):
-            if len(value) > 1:
-                print(f"|{value} |".rjust(3),end=' ')
-            else:
-                print(f"| {value} |".rjust(4),end=' ')
-            if account in (3,7,11,15):
+def decorator(function):
+    def printPuzzle(obj,*args):
+        function(obj,*args)
+        # printing the puzzle
+        for account,rows in enumerate(obj.puzzle_board):
+            print("\n-----------------------")
+            for element in rows:
+                if len(element) > 1:
+                    print(f"|{element} |".rjust(4),end=' ')
+                else:
+                    print(f"| {element} |".rjust(4),end=' ')
+            if account == 3:
                 print("\n-----------------------")
+    return printPuzzle
 
+def shuffleItems(function):
+    def shuffle(obj):
+        from random import shuffle
+        shuffle(obj.puzzle_board)
+        for sublist in obj.puzzle_board:
+            shuffle(sublist)
+        function(obj)
+    return shuffle
+            
+class Puzzle:
+    def __init__(self):
+        self.puzzle_board = []
+        self.EMPTY = ' '
+    # check this
+    @decorator
+    @shuffleItems
+    def fillBoard(self):
+        new_list = list()
+        account = 0
+        for account, number in enumerate(range(1,17),start=1):
+            number = str(number) if account != 16 else self.EMPTY
+            new_list.append(number)
+            if account in (4,8,12,16):
+                self.puzzle_board.append(new_list)
+                new_list = list()
+    
+    def findItem(self, value=' '):
+        row, col = 0,0
+        for rows in range(len(self.puzzle_board)):
+            if value in self.puzzle_board[rows]:
+                row, col = rows, self.puzzle_board[rows].index(value)
+                break
+        return row, col
+    
+    @decorator
+    def swapItems(self, value):
+        
+        empty_row, empty_col = self.findItem(self.EMPTY)
+        value_row, value_col = self.findItem(value)
+        patterns = [(empty_row - 1,empty_col),(empty_row + 1,empty_col),(empty_row,empty_col -1),(empty_row,empty_col + 1)]
+        
+        if (value_row,value_col) in patterns:
+            self.puzzle_board[value_row][value_col], self.puzzle_board[empty_row][empty_col] = self.puzzle_board[empty_row][empty_col],self.puzzle_board[value_row][value_col]
+        else:
+            print("No se puede chavo")
 
-newpuzzle = Puzzle()
-newpuzzle.showArray()
-newpuzzle.fillPuzzle()
-newpuzzle.showArray()
-newpuzzle.shuffleItems()
-print(f"\nDictionary: {newpuzzle.establishKeys()}")
-# newpuzzle.showArray()
-# newpuzzle.showArray()
-newpuzzle.showPuzzle()
-print()
+    
+    
+
+new_puzzle = Puzzle()
+new_puzzle.fillBoard()
+new_puzzle.swapItems('8')
+new_puzzle.swapItems('15')
+new_puzzle.swapItems('14')
+new_puzzle.swapItems('13')
+new_puzzle.swapItems('10')
