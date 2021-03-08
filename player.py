d13ae9261acb414f19ec2f7afbb0b3d6bb3c0d8e
David Isaac Alvarez Garcia <alchemistcodepython@gmail.com>
2021-03-07 21:45:53 -0600

    changing player

--- player.py
+++ player.py
@@ -1,29 +1,32 @@
 class Player:
     
-    def __init__(self, player_name="Player None"):
+    def __init__(self, player_name="Player 1",number="1"):
         self.__name = player_name
+        self.__number = number
+    
     # getter player name
     @property
     def playerName(self):
         return self.__name
     # setter player name
-    @playerName.setter
-    def playerName(self, player_name):
-        self.__name = player_name
+    # @playerName.setter
+    # def playerName(self, player_name):
+    #     self.__name = player_name
     # deleter player name
     @playerName.deleter
     def playerName(self):
         del self.__name
     
     @property
-    def playerNumber(self):
-        return self.__number_player
+    def number(self):
+        return self.__number
     
-    @playerNumber.setter
-    def playerNumber(self, number_player):
+    @number.setter
+    def number(self, number):
+        # Cambialo por un while para comprobar si el valor esta dentro del rango o si es el numero ingresado
         inside_of_board = [str(numbers) for numbers in range(1,16)]
-        if self.__number_player in inside_of_board:
-            self.__number_player = number_player
+        if self.__number in inside_of_board:
+            self.__number = number
         else:
             raise TypeError("Expected a number from the board!")
     
